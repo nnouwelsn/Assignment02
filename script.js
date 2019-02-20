3efd1bb986d986b998f5a28c0f2653785c2c5c63
Nicole Nouwels <nicole.n.nouwels@gmail.com>
2019-02-19 16:26:23 -0800

    Assignment02 comm644

---
+++ script.js
@@ -0,0 +1,80 @@
+/*eslint-env browser*/
+
+/*var coinFlip = Math.random();
+
+var choice = window.prompt("Choose Heads or Tails");
+
+if(coinFlip < 100 && choice == "Heads"){
+    alert("The result was Heads and you chose Heads...you win!")
+} else if (coinFlip < 100 && choice == "Tails"){
+    alert("The result was Heads and you chose Tails...you lose!")
+} else if (coinFlip < 100 && choice == "Tails"){
+    alert("The result was Tails and you chose Tails...you win!")
+} else if (coinFlip < 100 && choice == "Heads"){
+    alert("The result was Tails and you chose Heads...you lose!")
+} */
+
+/*
+coinFlip game redux
+
+var coinFlip;
+for (var i = 1; i < 10; i++) {
+    coinFlip = Math.round(Math.random());
+    if (coinFlip == 0) {
+        window.console.log("Heads");
+    }
+    if (coinFlip == 1) {
+        window.console.log("Tails");
+    }
+}
+*/
+
+
+//coinflip STREAK
+
+/*var coinFlip;
+do{
+    coinFlip = Math.round(Math.random());
+    if(coinFlip == 0){
+        window.console.log("Heads")
+    }
+    else if(coinFlip == 1)
+        window.console.log("Tails")
+}
+while(coinFlip == 1)*/
+    
+/*LOOPING A TRIANGLE
+
+for (var i = '#'; i.length < 7; i = i + '#') {
+  window.console.log(i);
+}*/
+
+
+/*ODD OR EVEN
+
+var x;
+
+for(x = 0; x < 15; x++) {
+    if (x % 2 === 0){
+        window.console.log("even");
+    }
+    else{
+        window.console.log("odd");
+    }
+}*/
+
+//MARCO POLO
+
+/*var i;
+
+for (i = 1; i < 100 ; i++) {
+if (i%5 == 0 && i%3 == 0){ 
+        document.write("Marco Polo! <br>")
+    }
+    else if (i % 5 == 0){
+            document.write("Marco! <br>")
+        }  else if (i % 3 == 0){
+            document.write("Polo! <br>")
+        }*/
+    
+    }
